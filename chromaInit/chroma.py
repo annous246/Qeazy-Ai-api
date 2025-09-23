0b792b975e40abfc5a02d7e1556c8ac6ff8dc643
annous246 <anasrabhi0@gmail.com>
2025-09-23 17:21:41 +0100

    reducing to smaller model

--- chromaInit/chroma.py
+++ chromaInit/chroma.py
@@ -1,11 +1,28 @@
 import chromadb
 from chromadb.config import Settings
+from chromadb.utils import embedding_functions
 import os
 from .categories import categories_list
+
+
 folder = os.path.join(os.getcwd(),"chromaInit")
+
+
+_embeddings = None
+def get_embeddings():
+    global _embeddings
+    if _embeddings is None:
+        print("Loading embedding model...")
+        _embeddings = embedding_functions.HuggingFaceEmbeddingFunction(
+            model_name="sentence-transformers/paraphrase-MiniLM-L3-v2"  # smaller model
+        )
+    return _embeddings
+
+
+
 client = chromadb.PersistentClient(path=folder)
 
-collection = client.get_collection("categories")
+collection = client.get_or_create_collection("categories")
 collection.upsert(
    documents = categories_list
 ,
